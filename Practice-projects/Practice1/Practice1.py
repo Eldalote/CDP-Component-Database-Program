d435d3455017333f8d0d980c290d9b0a03678b2c
Eldalote.Electronics <Eldalote.Electronics@gmail.com>
2021-03-25 14:18:25 +0100

    More practice (databases)
    Signed-off-by: Eldalote.Electronics <Eldalote.Electronics@gmail.com>

--- Practice-projects/Practice1/Practice1.py
+++ Practice-projects/Practice1/Practice1.py
@@ -1,27 +1,60 @@
 from tkinter import * 
-
-root = Tk()
-
-labelText = StringVar()
-labelText.set("")
-
-entryField = Entry(root, width= 70)
-entryField.grid(row=0, column=0)
-entryField.insert(0, "Name here")
-
-myLabel = Label(root, textvariable=labelText)
-myLabel.grid(row=2, column = 0)
+import tkinter.messagebox
+import sqlite3
+from sqlite3 import Error
 
 
-def myClick():
-	labelText.set("Hello " + entryField.get())	
-	
+
+def create_db_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        tkinter.messagebox.showerror("Database connection error", "Error message: " + str(e))    
+
+    return conn
+        
+def sql_execute(conn, sql_command):
+    """Execute sql commands from sql_command 
+    :param conn: Connection object
+    :param sql_command: an SQL statement
+    :return:
+    """ 
+    try:
+        c = conn.cursor()
+        c.execute(sql_command)
+    except Error as e:
+        tkinter.messagebox.showerror("Database table error", "Error message: " + str(e))
+    
+sql_create_table_resistors = """ CREATE TABLE IF NOT EXISTS resistors (
+                                    id integer PRIMARY KEY,
+                                    name text,
+                                    value integer,
+                                    value_multiplier text,
+                                    footprint text,
+                                    tollerance text,
+                                    power_rating text                                    
+                             );"""
 
 
-clickButton = Button(root, text="Enter your name", command=myClick)
 
-clickButton.grid(row=1, column=0)
+firstWindow = Tk()
 
-root.mainloop()
+
+db_conn = create_db_connection(r"db/components.db")
+if db_conn is not None:
+    sql_execute(db_conn, sql_create_table_resistors)
+
+else:
+    tkinter.messagebox.showerror("Database error", "Some kind of error ")
+
+
+
+
+db_conn.commit()
+db_conn.close()
+firstWindow.mainloop()
 
 
